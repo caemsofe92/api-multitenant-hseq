c08f272f2af3316afcd2c0c1308f330f57e28593
carlos.soto <carlos.soto@srfconsultores.com>
2022-03-28 16:02:57 -0500

    Commit nuevo

---
+++ app.js
@@ -0,0 +1,27 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/entities');
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/entities', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.json('error');
+});
+
+module.exports = app;
