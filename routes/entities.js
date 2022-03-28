21a5fcc73468011fe51bc67ff2584bcb7f886aba
carlos.soto <carlos.soto@srfconsultores.com>
2022-03-28 17:53:48 -0500

    Primera Alfa

--- routes/entities.js
+++ routes/entities.js
@@ -3,32 +3,53 @@ var router = express.Router();
 const client = require('../bin/redis-client');
 const axios = require("axios");
 
-/* GET users listing. */
 router.get('/', async(req, res, next) => {
-  
-    // Search Data in Redis
-    const reply = await client.get("character");
 
-    // if exists returns from redis and finish with response
+    const reply = await client.get("SRFSecurityRoles");
+
     if (reply) return res.send(JSON.parse(reply));
 
-    // Fetching Data from Rick and Morty API
-    const response = await axios.get(
-      "https://rickandmortyapi.com/api/character"
-    );
+    var token = await client.get("token");
+    if (!token){
+      const optionsToken = {
+        method: 'post',
+        url: 'https://login.microsoftonline.com/gecolsa.com.co/oauth2/token',
+        data: "grant_type=client_credentials&client_id=74ca5bcf-dcce-4651-99a2-307c1b481046&client_secret=I-_7Q~Q42D4oMfPY0NxgKcl1PE3PhBj-OTRh5&resource=https://dfo365-preprod.sandbox.operations.dynamics.com",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        transformResponse: [async (data) => {
+          const _data = JSON.parse(data);
+          token = _data.access_token;
+          await client.set(
+            "token",
+            JSON.stringify(_data.access_token),
+            {
+              EX: 3599,
+            }
+          );
+          return data;
+        }]
+      };
+      await axios(optionsToken);
+    }
 
-    // Saving the results in Redis. The "EX" and 10, sets an expiration of 10 Seconds
-    const saveResult = await client.set(
-      "character",
-      JSON.stringify(response.data),
-      {
-        EX: 10,
-      }
-    );
-    console.log(saveResult)
+    const optionsEntity = {
+      method: 'get',
+      url: 'https://dfo365-preprod.sandbox.operations.dynamics.com/data/SRFSecurityRoles?$format=application/json;odata.metadata=none',
+      headers: {'Authorization': "Bearer " + token},
+      data: {},
+      transformResponse: [async (data) => {
 
-    // resond to client
-    res.send(response.data);
+        await client.set(
+          "SRFSecurityRoles",
+          JSON.stringify(data),
+          {
+            EX: 3599,
+          }
+        );
+        res.send(JSON.parse(data));
+      }]
+    };
+    await axios(optionsEntity);
   
 });
 
