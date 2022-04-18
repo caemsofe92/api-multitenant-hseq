140c55f9f080cfc1feae556dacc555665c75a60e
carlos.soto <carlos.soto@srfconsultores.com>
2022-04-18 08:32:04 -0500

    Aumento de capacidad para imagenes

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ var getDiagnostic = require("./routes/get-diagnostic");
 
 var app = express();
 app.use(compression());
-app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/", indexRouter);
 app.use("/create-raic", createRAICRouter);
