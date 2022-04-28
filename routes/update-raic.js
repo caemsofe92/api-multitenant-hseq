b2201ee2d121a6bf91a76a05f1d12ed133cbc1d4
carlos.soto <carlos.soto@srfconsultores.com>
2022-04-28 10:33:25 -0500

    Commit

--- routes/update-raic.js
+++ routes/update-raic.js
@@ -435,7 +435,7 @@ router.post("/", async (req, res) => {
       }
     }
 
-    if (email) {
+    if (email && unsafeCondition.State === "Close") {
       await axios
         .post(
           "https://prod-60.westus.logic.azure.com:443/workflows/ff6b14da6ee9444fb7f3c46b4558981b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Ba7NYh2lQRCXvSaz6xMQXKHGrQ1QWl48svmf6NS-c9c",
