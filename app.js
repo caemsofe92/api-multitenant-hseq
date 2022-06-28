fe8a3ebc6c339e07fba05856a0d8ae4d1c18e766
Carlos Soto <carlos.soto@srfconsultores.com>
2022-06-27 23:20:28 -0500

    Commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var getFilterRaic = require("./routes/get-filtered-raic");
 var getDiagnostic = require("./routes/get-diagnostic");
 var getWorkers = require("./routes/get-workers-number");
 var calculatePaginationList = require("./routes/calculate-pagination-list");
-var ping = require("./routes/ping");
+var getPing = require("./routes/get-ping");
 
 var app = express();
 app.use(compression());
@@ -33,7 +33,7 @@ app.use("/get-diagnostic", getDiagnostic);
 app.use("/get-filtered-raic", getFilterRaic);
 app.use("/get-workers-number", getWorkers);
 app.use("/calculate-pagination-list", calculatePaginationList);
-app.use("/ping", ping);
+app.use("/get-ping", getPing);
 
 app.use(function (req, res, next) {
   next(createError(404));
